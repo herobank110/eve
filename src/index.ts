21cde87d69b8db34cfc1781a6acee6d926e98d9b
David Kanekanian <dkanekanian1@gmail.com>
2022-11-05 12:58:12 +0000

    animated bg

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-$(window).on('scroll', e => {
-  console.log(window.scrollY);
+function lerp1(a: number, b: number, x: number) {
+  return a + (b - a) * x;
+}
+
+$(window).on('scroll', () => {
+  const percent = window.scrollY / window.innerHeight;
+  $(document.body).css('background', `hsl(0, 0%, ${lerp1(0, 100, percent)}%)`);
 });
