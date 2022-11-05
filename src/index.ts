bc9c7c93cc3dfc8deb83d46ac3330a9c96e67054
David Kanekanian <dkanekanian1@gmail.com>
2022-11-05 13:04:09 +0000

    non jquery?

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ function lerp1(a: number, b: number, x: number) {
   return a + (b - a) * x;
 }
 
-$(window).on('scroll', () => {
+window.addEventListener('scroll', () => {
+  // $(window).on('scroll', () => {
   const percent = window.scrollY / window.innerHeight;
   $(document.body).css('background', `hsl(0, 0%, ${lerp1(0, 100, percent)}%)`);
+  document.body.style.background = `hsl(0, 0%, ${lerp1(0, 100, percent)}%)`;
 });
