84420ad738b4dd7818feb1c1280011c0596bbed3
David Kanekanian <dkanekanian1@gmail.com>
2022-11-05 15:14:24 +0000

    animaed opac

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,37 @@ function lerp1(a: number, b: number, x: number) {
 $(window).on('scroll', () => {
   const percent = window.scrollY / window.innerHeight;
   $(document.body).css('background', `hsl(0, 0%, ${lerp1(0, 100, percent)}%)`);
+  $('html').css(R.arrowOpac, lerp1(80, 20, percent) + '%');
 });
 
 $(() => {
-    $(".cent").append(makeArrow())
-    $(".cent").html($(".cent").html()) // reload for svg
-})
+  $('.scr').append(makeArrow());
+  $('.scr').html($('.scr').html()); // reload for svg
+});
+
+const R = {
+  arrowOpac: '--arrowOpac',
+} as const;
+
+function get(name: string) {
+  return `var(${name})`;
+}
+
+function set(name: string, value: string | number, unit?: string) {
+  if (unit) value = value.toString() + unit;
+  $('html').css(name, value);
+}
 
 const makeArrow = () =>
-  $('<svg>', { width: 100, height: 200 }).append(
+  $('<svg>', {
+    css: { width: 100, height: 200, opacity: get(R.arrowOpac) },
+  }).append(
     $('<line>', {
       x1: '50',
       y1: '20',
       x2: '50',
       y2: '148',
-      style: 'stroke:#ccc;stroke-width:2;',
+      style: `stroke:#ccc;stroke-width:2;`,
     }),
     $('<polygon>', {
       points: '20,140, 80,140, 50,160',
