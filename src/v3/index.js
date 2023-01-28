98e586277ba058b5e437edb7192adb61266138fa
David Kanekanian <dkanekanian1@gmail.com>
2023-01-28 23:35:38 +0000

    fixed animated vignette

--- src/v3/index.js
+++ src/v3/index.js
@@ -1,17 +1,12 @@
 window.onload = () => {
   document.querySelector('.splashVignette').animate(
-    { background: [
-
-  'radial-gradient(circle, transparent 50%, transparent 150%)',
-  'radial-gradient(circle, transparent 50%, red 150%)'
-
-    ] },
+    { opacity: [0, 1] },
     {
       timeline: new ViewTimeline({
         subject: document.querySelector('.splash'),
         orientation: 'block',
       }),
-      timeRange: 'exit',
+      timeRange: 'exit 0% 50%',
       fill: 'both',
     },
   );
