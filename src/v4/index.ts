c475faecbd2df0fcbe762393566177bf64668238
David Kanekanian <dkanekanian1@gmail.com>
2023-04-25 13:21:36 +0100

    use text

--- src/v4/index.ts
+++ src/v4/index.ts
@@ -1,6 +1,8 @@
-import anims from "./anims/regenerate";
-import lottie from "lottie-web";
+import anims from './anims';
+import lottie from 'lottie-web';
+console.log(anims);
+
 lottie.loadAnimation({
-    container: document.body,
-    path: "./"
+  container: document.body,
+  animationData: anims.VRW_010
 });
