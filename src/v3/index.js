a3fb4b95aba4b7d236aa6607c00be256c0830f28
David Kanekanian <dkanekanian1@gmail.com>
2023-05-13 17:54:48 +0100

    scroll experimentation

--- src/v3/index.js
+++ src/v3/index.js
@@ -1,6 +1,6 @@
 window.onload = () => {
   document.querySelector('.splashVignette').animate(
-    { opacity: [0, 1] },
+    { opacity: [0, 1, 1] },
     {
       timeline: new ViewTimeline({
         subject: document.querySelector('.splash'),
@@ -10,4 +10,16 @@ window.onload = () => {
       fill: 'both',
     },
   );
+
+  // document.querySelector('.splash').animate(
+  //   { position: ["fixed", "fixed"] },
+  //   {
+  //     timeline: new ViewTimeline({
+  //       subject: document.querySelector('.1'),
+  //       orientation: 'block',
+  //     }),
+  //     timeRange: 'exit',
+  //     fill: 'both',
+  //   },
+  // );
 };
