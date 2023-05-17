34614de6795dcdbca5902472de768536802abf45
David Kanekanian <dkanekanian1@gmail.com>
2023-05-17 16:20:53 +0100

    refactor

--- src/v3/index.ts
+++ src/v3/index.ts
@@ -1,5 +1,51 @@
 import LilGui from 'lil-gui';
 
+// helper functions
+
+function queryAncestor(el: Element, selector: string) {
+  let fl: Element | null = el;
+  do fl = fl!.parentElement;
+  while (fl && !fl?.matches(selector));
+  return fl;
+}
+
+const animate = (
+  selector: string,
+  keyframes: Keyframe[] | PropertyIndexedKeyframes,
+  options: KeyframeAnimationOptions,
+) => {
+  const animEl = document.querySelector(selector);
+  if (!animEl) throw new Error(`Invalid element for selector: ${selector}`);
+  const trackEl = queryAncestor(animEl, '.t');
+  if (!trackEl) throw new Error(`Couldn't find track for selector ${selector}`);
+  const timeline = new ViewTimeline({ subject: trackEl });
+  animEl.animate(keyframes, { ...options, timeline });
+};
+
+// create the animations
+
+animate(
+  '.splashVignette',
+  { opacity: [0, 1] },
+  { timeRange: 'exit 15% 35%', fill: 'both', easing: 'ease-in' },
+);
+animate(
+  '.splash',
+  { position: ['fixed', 'relative'], translate: ['', '0 24lvh'] },
+  { timeRange: 'exit 0% 30%', fill: 'both', easing: 'steps(1)' },
+);
+animate(
+  '.splash',
+  { translate: ['0 24lvh', '0 -20lvh'] },
+  {
+    timeRange: 'exit 30% 100%',
+    fill: 'forwards',
+    easing: 'cubic-bezier(0.3, 0.3, 0.1, 1)',
+  },
+);
+
+// apply debug gui
+
 if (window.location.hostname == 'localhost') {
   const debugGui = new LilGui();
   const debugSettings = {
@@ -12,56 +58,3 @@ if (window.location.hostname == 'localhost') {
     );
   });
 }
-
-const track = (index: number) =>
-  new (class {
-    private _animEl: Element | null = null;
-    private _keyframes: Keyframe[] | PropertyIndexedKeyframes | undefined;
-
-    animate(selector: string) {
-      // the element we are about to animate.
-      this._animEl = document.querySelector(selector);
-      if (!this._animEl)
-        throw new Error(`Invalid element for selector: ${selector}`);
-      return this;
-    }
-
-    keyframes(keyframes: Keyframe[] | PropertyIndexedKeyframes) {
-      this._keyframes = keyframes;
-      return this;
-    }
-
-    options(timeRange: AnimationTimeRange, fill: FillMode, easing: string) {
-      // track element for scroll percentages.
-      const trackEl = document.querySelector(`.t${index}`);
-      if (!trackEl) throw new Error(`Invalid element for track: ${index}`);
-
-      if (!this._animEl) throw new Error('Invalid anim element');
-
-      // set the animation. requires reloading the page every time it was changed
-      // as does not seem to work well with hot reload
-      const timeline = new ViewTimeline({ subject: trackEl });
-      this._animEl.animate(this._keyframes!, {
-        timeRange,
-        easing,
-        fill,
-        timeline,
-      });
-
-      // allow chaining
-      return this;
-    }
-  })();
-
-window.onload = () => {
-  track(1)
-    .animate('.splashVignette')
-    .keyframes({ opacity: [0, 1] })
-    .options('exit 15% 35%', 'both', 'ease-in')
-    .animate('.splash')
-    .keyframes({ position: ['fixed', 'relative'], translate: ['', '0 24lvh'] })
-    .options('exit 0% 30%', 'both', 'steps(1)')
-    .animate('.splash')
-    .keyframes({ translate: ['0 24lvh', '0 -20lvh'] })
-    .options('exit 30% 100%', 'forwards', 'cubic-bezier(0.3, 0.3, 0.1, 1)');
-};
