b02be536d454389316a8a5e3bed95be9cd466e76
David Kanekanian <dkanekanian1@gmail.com>
2023-05-17 16:06:05 +0100

    refactor a bit

--- src/v3/index.ts
+++ src/v3/index.ts
@@ -15,24 +15,37 @@ if (window.location.hostname == 'localhost') {
 
 const track = (index: number) =>
   new (class {
-    animate(
-      selector: string,
-      keyframes: Keyframe[] | PropertyIndexedKeyframes,
-      options: KeyframeAnimationOptions,
-    ) {
+    private _animEl: Element | null = null;
+    private _keyframes: Keyframe[] | PropertyIndexedKeyframes | undefined;
+
+    animate(selector: string) {
+      // the element we are about to animate.
+      this._animEl = document.querySelector(selector);
+      if (!this._animEl)
+        throw new Error(`Invalid element for selector: ${selector}`);
+      return this;
+    }
+
+    keyframes(keyframes: Keyframe[] | PropertyIndexedKeyframes) {
+      this._keyframes = keyframes;
+      return this;
+    }
+
+    options(timeRange: AnimationTimeRange, fill: FillMode, easing: string) {
       // track element for scroll percentages.
       const trackEl = document.querySelector(`.t${index}`);
       if (!trackEl) throw new Error(`Invalid element for track: ${index}`);
 
-      // the element we are about to animate.
-      const animEl = document.querySelector(selector);
-      if (!animEl) throw new Error(`Invalid element for selector: ${selector}`);
+      if (!this._animEl) throw new Error('Invalid anim element');
 
       // set the animation. requires reloading the page every time it was changed
       // as does not seem to work well with hot reload
-      animEl.animate(keyframes, {
-        ...options,
-        timeline: new ViewTimeline({ subject: trackEl }),
+      const timeline = new ViewTimeline({ subject: trackEl });
+      this._animEl.animate(this._keyframes!, {
+        timeRange,
+        easing,
+        fill,
+        timeline,
       });
 
       // allow chaining
@@ -42,23 +55,13 @@ const track = (index: number) =>
 
 window.onload = () => {
   track(1)
-    .animate(
-      '.splashVignette',
-      { opacity: [0, 1] },
-      { timeRange: 'exit 15% 35%', fill: 'both', easing: 'ease-in' },
-    )
-    .animate(
-      '.splash',
-      { position: ['fixed', 'relative'], translate: ['', '0 24lvh'] },
-      { timeRange: 'exit 0% 30%', easing: 'steps(1)', fill: 'both' },
-    )
-    .animate(
-      '.splash',
-      { translate: ['0 24lvh', '0 -20lvh'] },
-      {
-        timeRange: 'exit 30% 100%',
-        easing: 'cubic-bezier(0.3, 0.3, 0.1, 1)',
-        fill: 'forwards',
-      },
-    );
+    .animate('.splashVignette')
+    .keyframes({ opacity: [0, 1] })
+    .options('exit 15% 35%', 'both', 'ease-in')
+    .animate('.splash')
+    .keyframes({ position: ['fixed', 'relative'], translate: ['', '0 24lvh'] })
+    .options('exit 0% 30%', 'both', 'steps(1)')
+    .animate('.splash')
+    .keyframes({ translate: ['0 24lvh', '0 -20lvh'] })
+    .options('exit 30% 100%', 'forwards', 'cubic-bezier(0.3, 0.3, 0.1, 1)');
 };
